538133b963fedc7916b4ed4cacc3d1947054044f
afisher@advantageiq.com <afisher@ecova.com>
2021-10-26 09:33:44 -0400

    backend cleanup

--- rrg-backend/app.js
+++ rrg-backend/app.js
@@ -4,7 +4,6 @@ var cors = require('cors');
 
 // routes
 const records = require('./routes/api/records');
-
 const app = express();
 
 // Connect Database
@@ -16,11 +15,8 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello record world!'));
-
 // use Routes
 app.use('/api/records', records);
 
 const port = process.env.PORT || 8082;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
