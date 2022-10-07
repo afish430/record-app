f59a16b75476eb0b3c15f519cb7daf3a39ceb1fd
afisher@advantageiq.com <afish430@gmail.com>
2022-10-07 11:00:09 -0400

    style updates

--- rrg-react/src/components/AppHeader.js
+++ rrg-react/src/components/AppHeader.js
@@ -20,10 +20,10 @@ function AppHeader() {
             <div className="container">
                 <h1>The Vinylator<img src={turntable} alt="recordplayer"></img></h1>
                 <div className="row">
-                    <Link to="/" className={"btn float-left mr-2 " + (activeRoute === "Generate" ? "btn-info" : "btn-outline-light")} onClick={setGenerateActive}>
+                    <Link to="/" className={"btn mr-2 " + (activeRoute === "Generate" ? "btn-info" : "btn-outline-light")} onClick={setGenerateActive}>
                         Generate a Record!
                     </Link>
-                    <Link to="/manage-records" className={"btn float-left " + (activeRoute === "Manage" ? "btn-info" : "btn-outline-light")} onClick={setManageActive}>
+                    <Link to="/manage-records" className={"btn " + (activeRoute === "Manage" ? "btn-info" : "btn-outline-light")} onClick={setManageActive}>
                         Manage Records
                     </Link>
                 </div>
