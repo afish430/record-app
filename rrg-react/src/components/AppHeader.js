fe3d46f2292c517d376e45eef08a904f0449afb3
Arthur Fisher <afish430@gmail.com>
2022-11-11 18:46:43 -0500

    added react-bootstrap, info modal

--- rrg-react/src/components/AppHeader.js
+++ rrg-react/src/components/AppHeader.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import '../styles/App.scss';
 import turntable from './../images/turntable.png';
 
@@ -14,6 +16,10 @@ function AppHeader() {
     const setManageActive = () => {
         setActiveRoute('Manage');
     }
+
+    const [showModal, setShowModal] = useState(false);
+    const handleClose = () => setShowModal(false);
+    const handleShow = () => setShowModal(true);
   
     return (
         <div className="AppHeader text-center">
@@ -27,8 +33,32 @@ function AppHeader() {
                         Record Manager
                     </Link>
                 </div>
+                <Button variant="light" className="aboutBtn" onClick={handleShow}>
+                    About
+                </Button>
             </div>
             <hr></hr>
+
+            <Modal show={showModal} onHide={handleClose} centered>
+                <Modal.Header>
+                    <Modal.Title className="m-auto">
+                        The Vinylator
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    The Vinylator is your own personal record collection manager and random record generator.
+                    Add, edit and delete records to manage your vinyl collection.
+                    You can also easily search and filter your collection, and even mark certain records as favorites.
+                    When you just aren't sure what to listen to, the Random Record Generator feature is your best friend.
+                    Just hit the "Generate" button and let the music come to you. Happy listening!
+                </Modal.Body>
+                <Modal.Footer className="text-center">
+                    <Button variant="info" onClick={handleClose} className="m-auto">
+                        Rock On!
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
         </div>
     );
   };
